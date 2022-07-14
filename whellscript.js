f50f2538646088f0289047934b70e57d81153638
hoangdv4 <41949602+dvhoang@users.noreply.github.com>
2022-07-14 12:45:43 +0700

    add animation celebrate

---
+++ whellscript.js
@@ -0,0 +1,25 @@
+import confetti from "https://cdn.skypack.dev/canvas-confetti";
+
+const colors = [
+    getComputedStyle(document.body).getPropertyValue("--c1"),
+    getComputedStyle(document.body).getPropertyValue("--c2"),
+    getComputedStyle(document.body).getPropertyValue("--c3"),
+    getComputedStyle(document.body).getPropertyValue("--c4"),
+    getComputedStyle(document.body).getPropertyValue("--c5"),
+    getComputedStyle(document.body).getPropertyValue("--c6"),
+    getComputedStyle(document.body).getPropertyValue("--c7"),
+    ];
+
+    export function celebrate() {
+    confetti({
+        particleCount: 500,
+        startVelocity:40,
+        angle:-90,
+        spread: 360,
+        origin: { y: 0.5 },
+        colors,
+        disableForReducedMotion: false
+    });
+}
+
+//==========
